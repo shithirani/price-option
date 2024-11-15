42454258231ec730fb7be234975d9cda5f0fcc9a
shithi <shithisarker20@gmail.com>
2024-11-15 20:22:56 +0600

    first added

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import DaisiNav from './Components/Daisinav/DaisiNav'
+import Navber from './Components/Navber/Navber'
+
+function App() {
+
+  return (
+    <>
+   
+      <h1 className='text-7xl bg-rose-700'>Vite + React</h1>
+      <DaisiNav></DaisiNav>
+      <Navber></Navber>
+    </>
+  )
+}
+
+export default App
