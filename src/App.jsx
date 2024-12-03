e01a7910214519d1de644acdc19b88d10ee24e0a
shithi <shithisarker20@gmail.com>
2024-12-03 20:21:53 +0600

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+// import { LineChart } from 'recharts'
 import './App.css'
+import LineChart from './Components/LineChart/LineChart'
 // import DaisiNav from './Components/Daisinav/DaisiNav'
 import Navber from './Components/Navber/Navber'
 import PriceOptions from './Components/PriceOptions/PriceOptions'
@@ -10,6 +12,9 @@ function App() {
         <Navber></Navber>
       <h1 className='text-7xl bg-rose-700'>Vite + React</h1>
       <PriceOptions></PriceOptions>
+      
+      <LineChart></LineChart>
+
       {/* <DaisiNav></DaisiNav> */}
      
     </>
--- src/Components/LineChart/LineChart.jsx
+++ src/Components/LineChart/LineChart.jsx
@@ -1,61 +1,67 @@
-
+import { LineChart as LChart, Line,XAxis,YAxis } from 'recharts';
 const LineChart = () => {
     const mathMarks=[
         {
-          "id": 1,
+          id: 1,
           "name": "Student A",
-          "marks": 78
+          marks: 78
         },
         {
-          "id": 2,
+          id: 2,
           "name": "Student B",
-          "marks": 85
+          marks:85
         },
         {
-          "id": 3,
+          id: 3,
           "name": "Student C",
-          "marks": 92
+          marks: 92
         },
         {
-          "id": 4,
+          id: 4,
           "name": "Student D",
-          "marks": 66
+          marks: 66
         },
         {
-          "id": 5,
+          id: 5,
           "name": "Student E",
-          "marks": 74
+          marks: 74
         },
         {
-          "id": 6,
+          id: 6,
           "name": "Student F",
-          "marks": 88
+          marks: 88
         },
         {
-          "id": 7,
+          id:7,
           "name": "Student G",
-          "marks": 95
+          marks: 95
         },
         {
-          "id": 8,
+          id: 8,
           "name": "Student H",
-          "marks": 59
+          marks: 59
         },
         {
-          "id": 9,
+          id: 9,
           "name": "Student I",
-          "marks": 81
+          marks: 81
         },
         {
-          "id": 10,
+          id: 10,
           "name": "Student J",
-          "marks": 72
+          
+          marks: 72
         }
       ]
       
     return (
         <div>
-            
+            <LChart width={500} height={400} data={mathMarks}>
+              <XAxis dataKey="name"></XAxis>
+              <YAxis></YAxis>
+              <Line dataKey="marks"></Line>
+            </LChart>
+            {/* <h1>shithi</h1> */}
         </div>
     );
 };
